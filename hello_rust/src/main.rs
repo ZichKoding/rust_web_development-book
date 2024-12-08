bfb4a9a9e9995d3b1e3e3196256b0ff65bc1de28
ZichKoding <chriszichkocoding@gmail.com>
2024-12-08 12:40:17 -0500

    Adding if statements plus error handling.

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,10 +1,20 @@
 fn greeting(name: &str) -> String {
+    // Check if the name is empty. If so, raise an error message.
+    if name.is_empty() {
+        panic!("Name is required to have at least one character.");
+    }
+
     let format_greeting: String = format!("Hello, {}!", name);
     return format_greeting;
 }
 
 fn main() {
     let name: &str = "Chris";
+    let greeting_message: String = greeting(name);
+    println!("{}", greeting_message);
+
+    // Name is empty, so this will raise an error.
+    let name: &str = "";
     let greeting_message: String = greeting(name);
     println!("{}", greeting_message);
 }
@@ -19,5 +29,11 @@ mod tests {
         let expected: &str = "Hello, Chris!";
         let actual: String = greeting("Chris");
         assert_eq!(expected, actual);
+    }
+
+    #[test]
+    #[should_panic(expected = "Name is required to have at least one character.")]
+    fn test_greeting_empty() {
+        greeting("");
     }
 }
