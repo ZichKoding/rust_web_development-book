4e27a2f7a69a9943abe781ceb85c17f933474864
ZichKoding <chriszichkocoding@gmail.com>
2024-12-08 13:05:31 -0500

    Removed panic! error handling and now using the Result return for handling errors

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,22 +1,29 @@
-fn greeting(name: &str) -> String {
+fn greeting(name: &str) -> Result<String, String> {
     // Check if the name is empty. If so, raise an error message.
     if name.is_empty() {
-        panic!("Name is required to have at least one character.");
+        return Err("Name is required to have at least one character.".to_string());
+    } else if name.len() > 20 {
+        return Err("Name cannot be longer than 20 characters.".to_string());
     }
 
-    let format_greeting: String = format!("Hello, {}!", name);
-    return format_greeting;
+    return Ok(format!("Hello, {}!", name));
 }
 
 fn main() {
     let name: &str = "Chris";
-    let greeting_message: String = greeting(name);
-    println!("{}", greeting_message);
+    let greeting_message: Result<String, String> = greeting(name);
+    println!("{}", greeting_message.unwrap());
 
     // Name is empty, so this will raise an error.
     let name: &str = "";
-    let greeting_message: String = greeting(name);
-    println!("{}", greeting_message);
+    let greeting_message: Result<String, String> = greeting(name);
+    println!("{}", greeting_message.err().unwrap());
+
+    // Name is too long, so this will raise an error.
+    let name: &str = "ChrisChrisChrisChrisChris";
+    let greeting_message: Result<String, String> = greeting(name);
+    println!("{}", greeting_message.err().unwrap());
+
 }
 
 
@@ -27,13 +34,21 @@ mod tests {
     #[test]
     fn test_greeting() {
         let expected: &str = "Hello, Chris!";
-        let actual: String = greeting("Chris");
-        assert_eq!(expected, actual);
+        let actual:Result<String, String> = greeting("Chris");
+        assert_eq!(expected, actual.unwrap());
     }
 
     #[test]
-    #[should_panic(expected = "Name is required to have at least one character.")]
-    fn test_greeting_empty() {
-        greeting("");
+    fn test_greeting_empty_name() {
+        let expected: &str = "Name is required to have at least one character.";
+        let actual:Result<String, String> = greeting("");
+        assert_eq!(expected, actual.err().unwrap());
+    }
+
+    #[test]
+    fn test_greeting_long_name() {
+        let expected: &str = "Name cannot be longer than 20 characters.";
+        let actual:Result<String, String> = greeting("ChrisChrisChrisChrisChris");
+        assert_eq!(expected, actual.err().unwrap());
     }
 }
